8d95b4e37e288bdddc4db618b495df848dd67ccb
sheikhrafiqulislamrafi <sheikhrafiqulislam30@gmail.com>
2026-02-04 02:20:40 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Medical Chatbot',
+    name = 'Medical ChatBot',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Sheikh Rafiqul Islam Rafi',
+    author_email= 'sheikhrafiqulislam30@gmail.com',
     packages= find_packages(),
     install_requires = []
 
