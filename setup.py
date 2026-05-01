ba5a951df40a3207facce0b87c56d8ef894d0dd9
sheikhrafiqulislamrafi <sheikhrafiqulislam30@gmail.com>
2026-05-01 19:09:09 +0600

    Final submission

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot',
+    version= '0.0.0',
+    author= 'Sheikh Rafiqul Islam Rafi',
+    author_email= 'sheikhrafiqulislam30@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
